ac8bce801344ab88b4afffa740569709c43372d7
name <linhcm99999@gmail.com>
2021-08-15 16:44:27 +0700

    make it better

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -40,7 +40,6 @@ const userCtrl = {
             const url = `${CLIENT_URL}/user/activate/${activation_token}`
             sendMail(email, url, "Verify your email address")
 
-
             res.json({msg: "Register Success! Please activate your email to start."})
         } catch (err) {
             return res.status(500).json({msg: err.message})
