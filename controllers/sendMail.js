0a2b1390cac4e6d6c8c9378cb4669a92219715e9
name <linhcm99999@gmail.com>
2021-08-19 22:16:10 +0700

    update-activeta

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -1,46 +1,21 @@
-const nodemailer = require('nodemailer')
-const {google} = require('googleapis')
-const {OAuth2} = google.auth;
-const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground'
-
-const {
-    MAILING_SERVICE_CLIENT_ID,
-    MAILING_SERVICE_CLIENT_SECRET,
-    MAILING_SERVICE_REFRESH_TOKEN,
-    SENDER_EMAIL_ADDRESS
-} = process.env
-
-const oauth2Client = new OAuth2(
-    MAILING_SERVICE_CLIENT_ID,
-    MAILING_SERVICE_CLIENT_SECRET,
-    MAILING_SERVICE_REFRESH_TOKEN,
-    OAUTH_PLAYGROUND
-)
+const nodemailer = require("nodemailer");
 
 // send mail
 const sendEmail = (to, url, txt) => {
-    oauth2Client.setCredentials({
-        refresh_token: MAILING_SERVICE_REFRESH_TOKEN
-    })
+  const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "linhcm981a@gmail.com",
+      pass: "LINH123456q",
 
-    const accessToken = oauth2Client.getAccessToken()
-    const smtpTransport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: SENDER_EMAIL_ADDRESS,
-            clientId: MAILING_SERVICE_CLIENT_ID,
-            clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-            refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-            accessToken
-        }
-    })
+    },
+  });
 
-    const mailOptions = {
-        from: SENDER_EMAIL_ADDRESS,
-        to: to,
-        subject: "Activate-Email",
-        html: `
+  const mailOptions = {
+    from: "linhcm981a@gmail.com",
+    to: to,
+    subject: "Activate-Email",
+    html: `
             <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
             <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome .</h2>
             <p>Congratulations! You're almost set to start using MANAGER-STUDENT.
@@ -53,13 +28,13 @@ const sendEmail = (to, url, txt) => {
         
             <div>${url}</div>
             </div>
-        `
-    }
+        `,
+  };
+  console.log("activeta-send-email");
+  smtpTransport.sendMail(mailOptions, (err, info) => {
+    if (err) return err;
+    return infor;
+  });
+};
 
-    smtpTransport.sendMail(mailOptions, (err, infor) => {
-        if(err) return err;
-        return infor
-    })
-}
-
-module.exports = sendEmail
+module.exports = sendEmail;
