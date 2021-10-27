70dfa5871c86b99f612cb7641f611fbc70bb39f7
linhcm981a <linhcm981a@gmail.com>
2021-10-27 20:41:55 +0700

    up

---
+++ client/src/components/body/home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="home_page">
+     
+      <p>Xin chúc mừng! Bạn sắp bắt đầu sử dụng ứng dụng của chúng tôi.</p>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        My FB
+      </a>
+      <h3>heeeeeeeeeeeeeeeeeeee</h3>
+    </div>
+  );
+}
+
+export default Home;
---
+++ client/src/components/utils/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            404 | Not Found1
+        </div>
+    )
+}
+
+export default NotFound
